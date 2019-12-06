2bb04e45917783f84c8ec875b23a85723eaf0e20
stephenbilham <stephenbilham@yahoo.com>
2019-12-05 22:03:21 -0800

    looking at mobile responsiveness

---
+++ build/precache-manifest.3a2d5fd78e1f3eb333914348def1d7c9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bdd326fea196621c0ba5b41d9a3d4a94",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "e98e2dbb3e1f518e064c",
+    "url": "/Portfolio/static/css/main.e8c24495.chunk.css"
+  },
+  {
+    "revision": "5e192993f6cd6f7ea39f",
+    "url": "/Portfolio/static/js/2.1b63e0c2.chunk.js"
+  },
+  {
+    "revision": "e98e2dbb3e1f518e064c",
+    "url": "/Portfolio/static/js/main.2c400792.chunk.js"
+  },
+  {
+    "revision": "ff281b7297e200caa984",
+    "url": "/Portfolio/static/js/runtime-main.e39f2c8c.js"
+  }
+]);
